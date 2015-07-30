f5958626d5f76ccf79dafc72e6ccd3aa714691ae
RahulW <rahulwali@gmail.com>
2015-07-30 12:39:30 +0530

    testing 01

--- slider.h
+++ slider.h
@@ -4,7 +4,8 @@
 //
 //  Created by cirmac3 on 6/23/15.
 //  Copyright (c) 2015 cirmac3. All rights reserved.
-//
+// Rahul wali
+// Slider for the app will be used globally...
 
 #import <UIKit/UIKit.h>
 
